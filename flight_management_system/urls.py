282d35e4b4d6da9ef2cc5f81d3ec13e3899c4e04
atpial <atp82232@gmail.com>
2025-06-19 12:02:04 +0600

    feat: airline crud

--- flight_management_system/urls.py
+++ flight_management_system/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("user.urls")),
-    path("", include("flight.urls")),
+    path("airlines/", include("flight.urls")),
 ]
